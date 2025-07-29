17db38b6662b35a8240ceb0a9cd4fd1d89e422fb
MostafaBelala <mostafabelala15@gmail.com>
2025-07-29 15:26:06 +0300

    third modified revesion with Jonas (refactoring)

--- script.js
+++ script.js
@@ -1,26 +1,60 @@
 const workouts = document.querySelector(".workouts");
 const form = document.querySelector("form");
 const distanceInput = document.querySelector(".form__input--distance");
-const selectInput = document.querySelector(".form__input--type");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputTarget = document.querySelector(".form__input--target");
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + "").slice(-10);
+  static workoutsArray = [];
+
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cedence) {
+    super(coords, distance, duration);
+    this.cedence = cedence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// Application ARCHITECTURE
 class App {
   #map;
-  #coords;
   #markerCoords;
-  #type;
-  #distance;
-  #duration;
-  #target;
 
   constructor() {
     this._getPosition();
     form.addEventListener("submit", this._newWorkout.bind(this));
 
     // todo stitch b/t running, cycling
-    selectInput.addEventListener(
-      "change",
-      this._toggleElevationField.bind(this)
-    );
+    inputType.addEventListener("change", this._toggleElevationField.bind(this));
   }
 
   _getPosition() {
@@ -40,12 +74,12 @@ class App {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
 
-    this.#coords = [latitude, longitude];
+    const coords = [latitude, longitude];
 
     // -> Initialize the map
     // ^ Create a map instance and set its view to a given place and zoom level
     this.#map = L.map("map", {
-      center: this.#coords,
+      center: coords,
       zoom: 13,
     });
 
@@ -67,12 +101,12 @@ class App {
   }
 
   _toggleElevationField(e) {
-    this.#type = e.target.value;
+    const typeOfInputType = e.target.value;
     const target = document.querySelector(`.form__input--target`);
-    target.placeholder = type === "running" ? "steps/min" : "meters";
-    target.name = type === "running" ? "cadence" : "Elev Gain";
+    target.placeholder = typeOfInputType === "running" ? "steps/min" : "meters";
+    target.name = typeOfInputType === "running" ? "cadence" : "Elev Gain";
     document.querySelector(".traget-label").textContent =
-      type === "running" ? "cadence" : "Elev Gain";
+      typeOfInputType === "running" ? "cadence" : "Elev Gain";
   }
 
   _newWorkout(e) {
@@ -80,22 +114,21 @@ class App {
 
     const data = new FormData(form);
 
-    this.#type = data.get("type");
-    this.#distance = data.get("distance");
-    this.#duration = data.get("duration");
-    this.#target = data.get(this.#type === "running" ? "cadence" : "Elev Gain");
+    const type = data.get("type");
+    const distance = data.get("distance");
+    const duration = data.get("duration");
+    const target = data.get(type === "running" ? "cadence" : "Elev Gain");
 
     // todo Validate the inputs
     if (
-      isNaN(Number(this.#distance)) ||
-      isNaN(Number(this.#duration)) ||
-      isNaN(Number(this.#target))
+      isNaN(Number(distance)) ||
+      isNaN(Number(duration)) ||
+      isNaN(Number(target))
     ) {
       alert("Please fill in all fields correctly.");
       return;
     }
 
-    // todo add a leaflet with title formatted with current date (month day)
     const date = new Date();
     const formattedDate = new Intl.DateTimeFormat("en-US", {
       month: "long",
@@ -103,10 +136,10 @@ class App {
     }).format(date);
 
     const popup = L.popup({
-      content: `${this.#type} on ${formattedDate}`,
+      content: `${type} on ${formattedDate}`,
       autoClose: false,
       closeOnClick: false,
-      className: this.#type === "running" ? "running-popup" : "cycling-popup",
+      className: type === "running" ? "running-popup" : "cycling-popup",
     })
       .setLatLng(this.#markerCoords)
       .openOn(this.#map);
@@ -119,48 +152,70 @@ class App {
       .bindPopup(popup)
       .openPopup();
 
+    if (type === "running") {
+      const newRunningObj = new Running(
+        this.#markerCoords,
+        distance,
+        duration,
+        target
+      );
+      Workout.workoutsArray.push(newRunningObj);
+      console.log(Workout.workoutsArray);
+    } else {
+      const newCyclingObj = new Cycling(
+        this.#markerCoords,
+        distance,
+        duration,
+        target
+      );
+      Workout.workoutsArray.push(newCyclingObj);
+      console.log(Workout.workoutsArray);
+    }
+
     // todo delete the form after submission
-    document.querySelector(".form").remove();
+    inputDistance.value = inputDuration.value = inputTarget.value = "";
+
+    form.classList.add("hidden");
 
     // todo update the workouts ul with a new li with the data extracted from the form
-    document.querySelector("ul").addEventListener(
-      "click",
-      function (e) {
-        const workoutEl = e.target.closest(".workout");
-        if (!workoutEl) return;
-        const coordsArr = JSON.parse(workoutEl.dataset.coords);
+    // document.querySelector("ul").addEventListener(
+    //   "click",
+    //   function (e) {
+    //     const workoutEl = e.target.closest(".workout");
+    //     if (!workoutEl) return;
+    //     const coordsArr = JSON.parse(workoutEl.dataset.coords);
 
-        // todo scroll to the map container
-        this.#map.panTo(coordsArr, {
-          animate: true,
-          duration: 1, // in seconds
-        });
-        //
-      }
-        .bind(this)
-        .bind(this)
-    );
+    //     // todo scroll to the map container
+    //     this.#map.panTo(coordsArr, {
+    //       animate: true,
+    //       duration: 1, // in seconds
+    //     });
+    //     //
+    //   }
+    //     .bind(this)
+    //     .bind(this)
+    // );
 
-    workouts.insertAdjacentHTML(
-      "afterbegin",
-      `<li class="workout ${this.#type}" data-coords= "${JSON.stringify([
-        this.#coords.lat,
-        this.#coords.lng,
-      ])}">
-        <h3>${this.#type} on ${formattedDate}</h3>
-        <div class="activities">
-          <div class="activity">${this.#type === "running" ? "🏃‍♂️" : "🚴‍♀️"} ${
-        this.#distance
-      } <span>KM</span></div>
-          <div class="activity">⏱ ${this.#duration} <span>MIN</span></div>
-          <div class="activity">⚡️ ${this.#distance} <span>MIN/KM</span></div>
-          <div class="activity">${this.#type === "running" ? "🦶🏼" : "⛰"} ${
-        this.#target
-      } <span>SPM</span></div>
-        </div>
-      </li>
-          `
-    );
+    // workouts.insertAdjacentHTML(
+    //   "afterbegin",
+    //   `<li class="workout ${this.#type}" data-coords= "${JSON.stringify([
+    //     this.#coords.lat,
+    //     this.#coords.lng,
+    //   ])}">
+    //     <h3>${this.#type} on ${formattedDate}</h3>
+    //     <div class="activities">
+    //       <div class="activity">${this.#type === "running" ? "🏃‍♂️" : "🚴‍♀️"} ${
+    //     this.#distance
+    //   } <span>KM</span></div>
+    //       <div class="activity">⏱ ${this.#duration} <span>MIN</span></div>
+    //       <div class="activity">⚡️ ${this.#pace} <span>MIN/KM</span></div>
+    //       <div class="activity">${this.#type === "running" ? "🦶🏼" : "⛰"} ${
+    //     this.#target
+    //   } <span>SPM</span></div>
+    //     </div>
+    //   </li>
+    //       `
+    // );
   }
 }
 
